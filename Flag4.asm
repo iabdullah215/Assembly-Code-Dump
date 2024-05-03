6eb0612e80da64bb760b75b3db36ab4cbdfb16ad
!abdullah <muhammadabdullah8040@gmail.com>
2024-05-04 00:01:23 +0500

    Create Flag4.asm

---
+++ Flag4.asm
@@ -0,0 +1,16 @@
+; overflow flag 
+
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO,
+dwExitCode:DWORD
+.data
+ myVariable DWORD 16, 12, 43
+ sum DWORD ?
+.code
+main PROC
+ mov al, +127 
+ add al, 1 
+ INVOKE ExitProcess, 0 
+main ENDP
+END main
