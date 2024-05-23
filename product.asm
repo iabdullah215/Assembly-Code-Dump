08797fb5671a746d2c349b49e86b4bfc91b6ea24
!abdullah <muhammadabdullah8040@gmail.com>
2024-05-23 20:12:35 +0500

    Create product.asm

---
+++ product.asm
@@ -0,0 +1,6 @@
+; We already know that SHL performs unsigned multiplication efficiently when the multiplier is a power of 2.
+;  • You can factor any binary number into powers of 2.
+;  • For example, to multiply EAX * 36,
+;  • factor 36 into 32 + 4 and
+;  • use the distributive property of multiplication to carry out the operation.
+
