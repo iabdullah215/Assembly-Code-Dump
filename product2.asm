4651c17d385fdd26004ece5eee2bcfb3cba5a96c
!abdullah <muhammadabdullah8040@gmail.com>
2024-05-23 20:39:17 +0500

    Update product2.asm

--- product2.asm
+++ product2.asm
@@ -1,3 +1,25 @@
 ; Multiply AX by 26, using shifting and addition instructions.
 ;    • Hint: 26 = 16 + 8 + 2.
 
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO,
+dwExitCode:DWORD
+
+.code
+main PROC
+	
+	mov eax, 123
+	mov eax, ebx
+	mov ebx, ecx
+
+	shl eax, 4
+	shl ebx, 3
+	shl ecx, 1
+
+	add eax, ebx
+	add eax, ecx
+
+INVOKE ExitProcess, 0
+main ENDP
+END main
