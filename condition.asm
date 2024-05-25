5cf59efcccd5792fcac5142a522a7bc73f970988
!abdullah <muhammadabdullah8040@gmail.com>
2024-05-25 18:05:16 +0500

    Update condition.asm

--- condition.asm
+++ condition.asm
@@ -1,1 +1,26 @@
+INCLUDE Irvine32.inc
 
+.data
+	
+	first DWORD 10;
+	second DWORD 20;
+	message_one db "Comparing Second with First", 0Ah, 0
+	message_two db "Now Comparing First with Second", 0Ah, 0
+
+.code
+main PROC
+	
+	mov eax, first
+	mov ebx, second
+
+	cmp eax, ebx
+	mov edx, OFFSET message_one
+	call WriteString
+	
+	cmp ebx, eax
+	mov edx, OFFSET message_two
+	call WriteString
+
+INVOKE ExitProcess, 0
+main ENDP
+END main
